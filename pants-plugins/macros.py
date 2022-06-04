a9d30550fdddd23fe89a88bc4ed49b8eca84bcfc
Amanibhavam <iam@defn.sh>
2022-06-04 07:51:55 +0000

    set python version for defn

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -1,10 +1,11 @@
 # type: ignore
 
 def defn_sources(**kwargs):
+    kwargs['interpreter_constraints'] = [">=3.10,<4"]
     kwargs['resolve'] = 'defn'
     python_sources(**kwargs)
 
 def defn_binary(**kwargs):
     kwargs['interpreter_constraints'] = [">=3.10,<4"]
-    kwargs['resolve'] = "defn"
+    kwargs['resolve'] = 'defn'
     pex_binary(**kwargs)
