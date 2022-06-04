d0050e47cafebc945e96c0d00dff701ca7489633
Amanibhavam <iam@defn.sh>
2022-06-04 07:39:18 +0000

    macros for defn sources, binary

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -3,3 +3,8 @@
 def defn_sources(**kwargs):
     kwargs['resolve'] = 'defn'
     python_sources(**kwargs)
+
+def defn_binary(**kwargs):
+    kwargs['interpreter_constraints'] = [">=3.10,<4"]
+    kwargs['resolve'] = "defn"
+    pex_binary(**kwargs)
