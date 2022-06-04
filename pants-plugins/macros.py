41f6788ddc2c9ee80bacce5610c2e2f08e5c68cd
Amanibhavam <iam@defn.sh>
2022-06-04 08:03:15 +0000

    pants plugins use python 3.8 (system python)

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -11,3 +11,9 @@ def defn_binary(**kwargs):
     kwargs["interpreter_constraints"] = [">=3.10,<4"]
     kwargs["resolve"] = "defn"
     pex_binary(**kwargs)
+
+
+def pp_sources(**kwargs):
+    kwargs["interpreter_constraints"] = [">=3.8,<4"]
+    kwargs["resolve"] = "pants-plugins"
+    python_sources(**kwargs)
