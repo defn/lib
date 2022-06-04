15b7e2a0e57c178a479b20195e7915953ad3971d
Amanibhavam <iam@defn.sh>
2022-06-04 07:58:00 +0000

    manage pants plugins sources

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -1,11 +1,13 @@
 # type: ignore
 
+
 def defn_sources(**kwargs):
-    kwargs['interpreter_constraints'] = [">=3.10,<4"]
-    kwargs['resolve'] = 'defn'
+    kwargs["interpreter_constraints"] = [">=3.10,<4"]
+    kwargs["resolve"] = "defn"
     python_sources(**kwargs)
 
+
 def defn_binary(**kwargs):
-    kwargs['interpreter_constraints'] = [">=3.10,<4"]
-    kwargs['resolve'] = 'defn'
+    kwargs["interpreter_constraints"] = [">=3.10,<4"]
+    kwargs["resolve"] = "defn"
     pex_binary(**kwargs)
