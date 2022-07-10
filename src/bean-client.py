dd12371d0de9ac3488f9531d3e10aaf7dd9e73b8
Amanibhavam <iam@defn.sh>
2022-07-10 22:58:02 +0000

    working bean/client server

---
+++ src/bean-client.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import logging
+
+import grpc
+
+import defn.dev.legumes.v1.bean_pb2 as bean_pb2
+import defn.dev.legumes.v1.bean_pb2_grpc as bean_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = bean_pb2_grpc.BeanStoreServiceStub(channel)
+        response = stub.GetBean(bean_pb2.Bean(url="hello", sha256="world"))
+    print("Bean client received: " + response.url + " " + response.sha256)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    run()
