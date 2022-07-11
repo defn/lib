5a2b047651d6bf80ecf65678daf9cee6380938d4
Amanibhavam <iam@defn.sh>
2022-07-11 01:39:14 +0000

    bean operator

---
+++ src/bean-oper.py
@@ -0,0 +1,13 @@
+import logging
+
+import grpc
+import kopf
+from google.protobuf.json_format import Parse, ParseDict
+
+import defn.dev.legumes.v1.bean_pb2 as bean_pb2
+
+
+@kopf.on.create("beans")  # type: ignore
+def create_fn(body, **kwargs):
+    bean = ParseDict(body.spec, bean_pb2.Bean())
+    logging.info(f"A handler is called with bean: url={bean.url}, sha256={bean.sha256}")
