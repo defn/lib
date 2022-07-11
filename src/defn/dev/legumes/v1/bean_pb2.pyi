3dc7c2e68c90b76b9027e739726c80955ac969f1
Amanibhavam <iam@defn.sh>
2022-07-11 05:21:20 +0000

    skip formatters in generated dirs

--- src/defn/dev/legumes/v1/bean_pb2.pyi
+++ src/defn/dev/legumes/v1/bean_pb2.pyi
@@ -1,8 +1,6 @@
-from typing import ClassVar, Optional, Text
-
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-
+from typing import ClassVar, Optional, Text
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -12,6 +10,4 @@ class Bean(_message.Message):
     URL_FIELD_NUMBER: ClassVar[int]
     sha256: str
     url: str
-    def __init__(
-        self, url: Optional[str] = ..., sha256: Optional[str] = ...
-    ) -> None: ...
+    def __init__(self, url: Optional[str] = ..., sha256: Optional[str] = ...) -> None: ...
