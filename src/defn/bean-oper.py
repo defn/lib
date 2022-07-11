1f18b99333a4362baee7838570640c684c7ff723
Amanibhavam <iam@defn.sh>
2022-07-11 05:06:30 +0000

    beans run as pex binaries

--- src/defn/bean-oper.py
+++ src/defn/bean-oper.py
@@ -2,6 +2,7 @@ import logging
 
 import kopf
 from google.protobuf.json_format import ParseDict
+from kopf import cli
 
 import defn.dev.legumes.v1.bean_pb2 as bean_pb2
 
@@ -10,3 +11,11 @@ import defn.dev.legumes.v1.bean_pb2 as bean_pb2
 def create_fn(body, **kwargs):
     bean = ParseDict(body.spec, bean_pb2.Bean())
     logging.info(f"A handler is called with bean: url={bean.url}, sha256={bean.sha256}")
+
+
+def main():
+    cli.run(["--all-namespaces", "--verbose", __file__])
+
+
+if __name__ == "__main__":
+    main()
