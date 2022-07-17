789881e55f27feef4c82e57a3183682e35acd4e6
Amanibhavam <iam@defn.sh>
2022-07-17 22:26:38 +0000

    use list for options

--- src/defn/bean-client.py
+++ src/defn/bean-client.py
@@ -10,12 +10,22 @@ import defn.dev.legumes.v1.bean_pb2_grpc as bean_pb2_grpc
 
 def run():
     with grpc.insecure_channel(
-        os.environ.get("server","kourier-internal-x-kourier-system-x-vc1.vc1.svc:80"),
-        options=(("grpc.default_authority", os.environ.get("authority","hello.demo.svc.cluster.local")),),
+        os.environ.get("server", "kourier-internal-x-kourier-system-x-vc1.vc1.svc:80"),
+        options=[
+            (
+                "grpc.default_authority",
+                os.environ.get("authority", "hello.demo.svc.cluster.local"),
+            )
+        ],
     ) as channel:
         stub = bean_pb2_grpc.BeanStoreServiceStub(channel)
 
-        response: bean_pb2.Bean = stub.GetBean(bean_pb2.Bean(url=os.environ.get("url","cool"), sha256=os.environ.get("sha256","beans")))
+        response: bean_pb2.Bean = stub.GetBean(
+            bean_pb2.Bean(
+                url=os.environ.get("url", "cool"),
+                sha256=os.environ.get("sha256", "beans"),
+            )
+        )
         print(f"Bean client received: {response.url}, {response.sha256}")
 
 
