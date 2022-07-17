32f5f3e0f994a8f218308390f3bd715751ec5897
Amanibhavam <iam@defn.sh>
2022-07-17 20:09:28 +0000

    set authority to knative service

--- src/defn/bean-client.py
+++ src/defn/bean-client.py
@@ -12,13 +12,15 @@ b = '{"url": "hello", "sha256": "string"}'
 
 
 def run():
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel(
+        "kourier-internal_kourier-system_svc_80.mesh:80"
+    ) as channel:
         stub = bean_pb2_grpc.BeanStoreServiceStub(channel)
 
-        response: bean_pb2.Bean = stub.GetBean(ParseDict(a, bean_pb2.Bean()))
-        print("Bean client received: " + response.url + " " + response.sha256)
-
-        response: bean_pb2.Bean = stub.GetBean(Parse(b, bean_pb2.Bean()))
+        response: bean_pb2.Bean = stub.GetBean.with_call(
+            ParseDict(a, bean_pb2.Bean()),
+            metadata=(("authority", 'hello.demo.svc.cluster.local"')),
+        )
         print("Bean client received: " + response.url + " " + response.sha256)
 
 
