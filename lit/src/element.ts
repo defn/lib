0faa3b9cb7c9fd2def1ab7ce64cbf9602a9d66d7
Amanibhavam <iam@defn.sh>
2022-09-24 01:09:56 +0000

    resetCounter in a dedicated component

--- lit/src/element.ts
+++ lit/src/element.ts
@@ -25,14 +25,21 @@ export class MyElement extends SignalWatcher(ExtElement) {
                 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 @click=${() => counter.value++}>
             ${this.name} ${counter}
-            <!-- Heroicon name: mini/envelope -->
-            <svg class="ml-3 -mr-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
-                <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
             </button>
+        `;
+    }
+}
 
-            <input @input=${this.resetCounter} placeholder="Enter a number">
+@customElement("my-input")
+export class MyInput extends SignalWatcher(ExtElement) {
+    render() {
+        return html`
+            <input
+                class="p-2 m-4"
+                @input=${this.resetCounter} placeholder="Enter a number">
         `;
     }
 
@@ -40,5 +47,4 @@ export class MyElement extends SignalWatcher(ExtElement) {
         const input = event.target as HTMLInputElement;
         counter.value = parseInt(input.value);
     }
-
 }
