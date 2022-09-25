5a95ba81a7be78b50413ecf55aef677e6b5be9db
Amanibhavam <iam@defn.sh>
2022-09-25 22:37:49 +0000

    dont need caddy to serve

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                nested: resolve(__dirname, 'meh/index.html')
+                meh: resolve(__dirname, 'meh/index.html')
             }
         }
     }
