b085b51ad7588b4c32d0543074c106018fd871d9
Amanibhavam <iam@defn.sh>
2022-09-25 06:24:58 +0000

    load tailwind styles

--- src/Common.ts
+++ src/Common.ts
@@ -1,14 +1,15 @@
-import { LitElement, html } from "lit";
+import { LitElement, css, unsafeCSS, CSSResultGroup } from "lit";
 import { SignalWatcher } from './signal-watcher.js';
 import { signal } from '@preact/signals-core';
 
-import './index.css'
+import componentStyle from './index.css'
 
 export const counter = signal(1);
 export const input1 = signal(1);
 export const input2 = signal(1);
 
 export class ExtElement extends SignalWatcher(LitElement) {
+    static styles = css`${unsafeCSS(componentStyle)}`
     xcreateRenderRoot() {
         return this.shadowRoot;
     }
