9ca117f8b9fb98e4429e35d19f67ec1d9f98c1ed
Amanibhavam <iam@defn.sh>
2022-09-26 02:21:03 +0000

    serve at root, let proxy handle urls

--- cmd/defm/defm.go
+++ cmd/defm/defm.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
-	app.Get("/api", func(c *fiber.Ctx) error {
+	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
