86616a76ba576f513fe315d8d8673a7ec0272720
Amanibhavam <iam@defn.sh>
2022-09-28 00:46:25 +0000

    add k8s client to verify pants can compile

--- cmd/defn/defn.go
+++ cmd/defn/defn.go
@@ -5,9 +5,12 @@ import (
 	"time"
 
 	"github.com/defn/cloud/pkg/meh"
+
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 func main() {
+	fmt.Println(fake.NewSimpleClientset().CoreV1().Events(""))
 	fmt.Println(meh.Hello("pants 9"))
 	time.Sleep(86400 * time.Second)
 }
