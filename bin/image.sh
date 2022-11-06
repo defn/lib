cc67c81302a49bb4b5cac46341867f2c0500843c
Amanibhavam <iam@defn.sh>
2022-11-06 20:55:43 +0000

    each dist is its own temporary git repo

--- bin/image.sh
+++ bin/image.sh
@@ -7,6 +7,7 @@ image="$1"; shift
 
 mkdir -p "dist/image-${name}"
 cd "dist/image-${name}"
+git init || true
 rm -f flake.lock flake.nix
 cp ../../flake.{nix,lock} .
 git add -f --intent-to-add bin flake.nix flake.lock
@@ -14,7 +15,6 @@ git update-index --assume-unchanged bin flake.nix flake.lock
 sudo rm -rf nix
 mkdir -p nix/store
 n build
-git rm -f --cached bin flake.nix flake.lock
 time for a in $(nix-store -qR ./result); do rsync -ia $a nix/store/; done
 (echo '# syntax=docker/dockerfile:1'; echo FROM alpine; echo RUN mkdir -p /app; for a in nix/store/*/; do echo COPY --link "$a" "/$a/"; echo COPY bin /app/bin; done; echo ENTRYPOINT [ '"/app/bin"' ]) > Dockerfile
 echo "ENV PATH $(for a in nix/store/*/; do echo -n "/$a/bin:"; done)/bin" >> Dockerfile
