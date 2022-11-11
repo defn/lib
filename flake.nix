60c23c4aab0ea5063f492c111488af10afc4ac46
Amanibhavam <iam@defn.sh>
2022-11-11 17:25:12 +0000

    sugared

--- flake.nix
+++ flake.nix
@@ -1,66 +1,57 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    dev.url = "github:defn/pkg?dir=dev&ref=v0.0.14";
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    dev.url = github:defn/pkg?dir=dev&ref=v0.0.16;
+    wrapper.url = github:defn/pkg?dir=wrapper&ref=v0.0.16;
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , dev
-    }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    rec {
-      devShell =
-        pkgs.mkShell rec {
-          buildInputs = with pkgs; [
-            dev.defaultPackage.${system}
-            defaultPackage
-            go
-            gotools
-            go-tools
-            golangci-lint
-            gopls
-            go-outline
-            gopkgs
-            nodejs-18_x
-            rsync
-          ];
-        };
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        wrap = inputs.wrapper.wrap { other = inputs; inherit system; inherit pkgs; };
+        slug = "defn-cloud";
+        version = "0.0.1";
+        buildInputs = with pkgs; [
+          go
+          gotools
+          go-tools
+          golangci-lint
+          gopls
+          go-outline
+          gopkgs
+          nodejs-18_x
+          rsync
+        ];
+      in
+      rec {
+        devShell = wrap.devShell;
+        defaultPackage =
+          pkgs.stdenv.mkDerivation rec {
+            name = "${slug}-${version}";
 
-      defaultPackage =
-        with import nixpkgs { inherit system; };
-        stdenv.mkDerivation rec {
-          name = "${slug}-${version}";
 
-          slug = "defn-cloud";
-          version = "0.0.1";
+            src = ./.;
 
-          src = ./.;
+            dontUnpack = true;
 
-          dontUnpack = true;
+            installPhase = ''
+              mkdir -p $out/bin
+              for a in $src/y/*.go; do
+                dst="$(basename "''${a%.go}")"
+                cp $a $out/bin/$dst
+                sed 's#^// yaegi#\#!/usr/bin/env yaegi#' -i $out/bin/$dst
+              done
+            '';
 
-          installPhase = ''
-            mkdir -p $out/bin
-            for a in $src/y/*.go; do
-              dst="$(basename "''${a%.go}")"
-              cp $a $out/bin/$dst
-              sed 's#^// yaegi#\#!/usr/bin/env yaegi#' -i $out/bin/$dst
-            done
-          '';
+            propagatedBuildInputs = buildInputs;
 
-          propagatedBuildInputs = [ ];
-
-          meta = with lib; {
-            homepage = "https://defn.sh/${slug}";
-            description = "nix golang / tilt integration";
-            platforms = platforms.linux;
+            meta = with pkgs.lib; {
+              homepage = "https://defn.sh/${slug}";
+              description = "nix golang / tilt integration";
+              platforms = platforms.linux;
+            };
           };
-        };
-    });
+      });
 }
