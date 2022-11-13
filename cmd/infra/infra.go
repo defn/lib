4186b9842a81a7ec15255cc26522e2a8e735b503
Amanibhavam <iam@defn.sh>
2022-11-13 19:21:42 +0000

    alias jsii.String

--- cmd/infra/infra.go
+++ cmd/infra/infra.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	j "github.com/aws/jsii-runtime-go"
+	"github.com/aws/jsii-runtime-go"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 
 	"github.com/cdktf/cdktf-provider-aws-go/aws/v10/instance"
@@ -14,11 +14,15 @@ import (
 	"github.com/cdktf/cdktf-provider-tfe-go/tfe/v3/workspace"
 )
 
+func js(s string) *string {
+	return jsii.String(s)
+}
+
 func defnWorkspacesStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
-	tfe.NewTfeProvider(stack, j.String("tfe"), &tfe.TfeProviderConfig{
-		Hostname: j.String("app.terraform.io"),
+	tfe.NewTfeProvider(stack, js("tfe"), &tfe.TfeProviderConfig{
+		Hostname: js("app.terraform.io"),
 	})
 
 	return stack
@@ -27,16 +31,16 @@ func defnWorkspacesStack(scope constructs.Construct, id string) cdktf.TerraformS
 func TheStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
-	aws.NewAwsProvider(stack, j.String("aws"), &aws.AwsProviderConfig{
-		Region: j.String("us-west-1"),
+	aws.NewAwsProvider(stack, js("aws"), &aws.AwsProviderConfig{
+		Region: js("us-west-1"),
 	})
 
-	instance := instance.NewInstance(stack, j.String("compute"), &instance.InstanceConfig{
-		Ami:          j.String("ami-01456a894f71116f2"),
-		InstanceType: j.String("t2.micro"),
+	instance := instance.NewInstance(stack, js("compute"), &instance.InstanceConfig{
+		Ami:          js("ami-01456a894f71116f2"),
+		InstanceType: js("t2.micro"),
 	})
 
-	cdktf.NewTerraformOutput(stack, j.String("public_ip"), &cdktf.TerraformOutputConfig{
+	cdktf.NewTerraformOutput(stack, js("public_ip"), &cdktf.TerraformOutputConfig{
 		Value: instance.PublicIp(),
 	})
 
@@ -50,9 +54,9 @@ func main() {
 
 	workspaces := defnWorkspacesStack(app, "workspaces")
 	cdktf.NewCloudBackend(workspaces, &cdktf.CloudBackendProps{
-		Hostname:     j.String("app.terraform.io"),
-		Organization: j.String(tfc_org),
-		Workspaces:   cdktf.NewNamedCloudWorkspace(j.String("workspaces")),
+		Hostname:     js("app.terraform.io"),
+		Organization: js(tfc_org),
+		Workspaces:   cdktf.NewNamedCloudWorkspace(js("workspaces")),
 	})
 
 	accounts := []string{"test-1", "test-2"}
@@ -60,17 +64,17 @@ func main() {
 	for _, acc := range accounts {
 		ws_name := fmt.Sprintf("%s-ws", acc)
 
-		workspace.NewWorkspace(workspaces, j.String(ws_name), &workspace.WorkspaceConfig{
-			Name:          j.String(ws_name),
-			Organization:  j.String(tfc_org),
-			ExecutionMode: j.String("local"),
+		workspace.NewWorkspace(workspaces, js(ws_name), &workspace.WorkspaceConfig{
+			Name:          js(ws_name),
+			Organization:  js(tfc_org),
+			ExecutionMode: js("local"),
 		})
 
 		st := TheStack(app, acc)
 		cdktf.NewCloudBackend(st, &cdktf.CloudBackendProps{
-			Hostname:     j.String("app.terraform.io"),
-			Organization: j.String(tfc_org),
-			Workspaces:   cdktf.NewNamedCloudWorkspace(j.String(ws_name)),
+			Hostname:     js("app.terraform.io"),
+			Organization: js(tfc_org),
+			Workspaces:   cdktf.NewNamedCloudWorkspace(js(ws_name)),
 		})
 	}
 
