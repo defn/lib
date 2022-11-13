6a219dfdf836693ad6b20a59c1154e706edd0caa
Amanibhavam <iam@defn.sh>
2022-11-13 03:11:53 +0000

    update to main

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    dev.url = github:defn/pkg?dir=dev&ref=v0.0.50;
+    dev.url = github:defn/pkg?dir=dev&ref=v0.0.53;
     temporalite.url = github:defn/pkg?dir=temporalite&ref=v0.0.47;
     tilt.url = github:defn/pkg?dir=tilt&ref=v0.0.47;
     earthly.url = github:defn/pkg?dir=earthly&ref=v0.0.47;
@@ -8,51 +8,58 @@
   };
 
   outputs = inputs:
-    inputs.dev.eachDefaultSystem (system:
-      let
-        site = import ./config.nix;
-        pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
-        wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; inherit site; };
-        latest = import inputs.latest { inherit system; };
-      in
-      with site;
-      rec {
-        devShell = wrap.devShell;
-        defaultPackage = wrap.bashBuilder {
-          src = ./.;
+    inputs.dev.main {
+      inherit inputs;
 
-          installPhase = ''
-            set -exu
-            mkdir -p $out/bin
-            for a in $src/y/*.go; do
-              dst="$(basename "''${a%.go}")"
-              (
-                echo "#!/usr/bin/env yaegi"
-                echo
-                cat $a
-              ) > $out/bin/$dst
-              chmod 755 $out/bin/$dst
-            done
-          '';
+      config = rec {
+        slug = "defn-cloud";
+        version = "0.0.1";
+        homepage = "https://defn.sh/${slug}";
+        description = "cloud infra and services";
+      };
 
-          propagatedBuildInputs = with latest; [
-            rsync
-            go
-            gotools
-            go-tools
-            golangci-lint
-            gopls
-            go-outline
-            gopkgs
-            nodejs-18_x
-          ];
-        };
+      handler = { pkgs, wrap, system }:
+        let
+          latest = import inputs.latest { inherit system; };
+        in
+        rec {
+          devShell = wrap.devShell;
 
-        packages = {
-          go = wrap.nullBuilder {
-            propagatedBuildInputs = [ latest.bash ];
+          defaultPackage = wrap.bashBuilder {
+            src = ./.;
+
+            installPhase = ''
+              set -exu
+              mkdir -p $out/bin
+              for a in $src/y/*.go; do
+                dst="$(basename "''${a%.go}")"
+                (
+                  echo "#!/usr/bin/env yaegi"
+                  echo
+                  cat $a
+                ) > $out/bin/$dst
+                chmod 755 $out/bin/$dst
+              done
+            '';
+
+            propagatedBuildInputs = with latest; [
+              rsync
+              go
+              gotools
+              go-tools
+              golangci-lint
+              gopls
+              go-outline
+              gopkgs
+              nodejs-18_x
+            ];
+          };
+
+          packages = {
+            go = wrap.nullBuilder {
+              propagatedBuildInputs = [ latest.bash ];
+            };
           };
         };
-      }
-    );
+    };
 }
