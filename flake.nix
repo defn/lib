33f539f1367a15049c1d1fc0c1ceebd504770b42
Amanibhavam <iam@defn.sh>
2022-11-13 01:22:20 +0000

    use bashBuilder

--- flake.nix
+++ flake.nix
@@ -1,62 +1,53 @@
 {
   inputs = {
-    dev.url = github:defn/pkg?dir=dev&ref=v0.0.26;
-    temporalite-pkg.url = github:defn/pkg?dir=temporalite&ref=v0.0.4;
+    dev.url = github:defn/pkg?dir=dev&ref=v0.0.47;
+    temporalite.url = github:defn/pkg?dir=temporalite&ref=v0.0.47;
+    tilt.url = github:defn/pkg?dir=tilt&ref=v0.0.47;
+    earthly.url = github:defn/pkg?dir=earthly&ref=v0.0.47;
     latest.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   };
 
   outputs = inputs:
     inputs.dev.wrapper.flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
-        wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; };
-        latest = import inputs.latest { inherit system; };
-        bi = with latest; [
-          rsync
-          go
-          gotools
-          go-tools
-          golangci-lint
-          gopls
-          go-outline
-          gopkgs
-          nodejs-18_x
-        ];
         site = import ./config.nix;
+        pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
+        wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; inherit site; };
+        latest = import inputs.latest { inherit system; };
       in
       with site;
       rec {
         devShell = wrap.devShell;
-        defaultPackage = pkgs.stdenv.mkDerivation
-          rec {
-            name = "${slug}-${version}";
+        defaultPackage = wrap.bashBuilder {
+          src = ./.;
 
-            src = ./.;
+          installPhase = ''
+            set -exu
+            mkdir -p $out/bin
+            for a in $src/y/*.go; do
+              dst="$(basename "''${a%.go}")"
+              (
+                echo "#!/usr/bin/env yaegi"
+                echo
+                cat $a
+              ) > $out/bin/$dst
+              chmod 755 $out/bin/$dst
+            done
+          '';
 
-            dontUnpack = true;
+          propagatedBuildInputs = with latest; [
+            rsync
+            go
+            gotools
+            go-tools
+            golangci-lint
+            gopls
+            go-outline
+            gopkgs
+            nodejs-18_x
+          ];
+        };
 
-            installPhase = ''
-              set -exu
-              mkdir -p $out/bin
-              for a in $src/y/*.go; do
-                dst="$(basename "''${a%.go}")"
-                (
-                  echo "#!/usr/bin/env yaegi"
-                  echo
-                  cat $a
-                ) > $out/bin/$dst
-                chmod 755 $out/bin/$dst
-              done
-            '';
-
-            propagatedBuildInputs = bi;
-
-            meta = with pkgs.lib; with site; {
-              inherit homepage;
-              inherit description;
-              platforms = platforms.linux;
-            };
-          };
         packages = {
           go = pkgs.stdenv.mkDerivation
             rec {
