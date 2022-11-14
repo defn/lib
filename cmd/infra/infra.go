cdb32d4a7de4e27eb6f297f9162d69504ace88f4
Amanibhavam <iam@defn.sh>
2022-11-14 02:18:35 +0000

    all the aws orgs

--- cmd/infra/infra.go
+++ cmd/infra/infra.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 
-	"github.com/cdktf/cdktf-provider-aws-go/aws/v10/instance"
 	aws "github.com/cdktf/cdktf-provider-aws-go/aws/v10/provider"
 
 	tfe "github.com/cdktf/cdktf-provider-tfe-go/tfe/v3/provider"
@@ -29,20 +26,11 @@ func defnWorkspacesStack(scope constructs.Construct, id string) cdktf.TerraformS
 	return stack
 }
 
-func TheStack(scope constructs.Construct, id string) cdktf.TerraformStack {
+func TheStack(scope constructs.Construct, id string, region string, sso_region string, namespace string, org string, prefix string, domain string, sub_accounts []string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
-	aws.NewAwsProvider(stack, js("aws"), &aws.AwsProviderConfig{
-		Region: js("us-west-1"),
-	})
-
-	instance := instance.NewInstance(stack, js("compute"), &instance.InstanceConfig{
-		Ami:          js("ami-01456a894f71116f2"),
-		InstanceType: js("t2.micro"),
-	})
-
-	cdktf.NewTerraformOutput(stack, js("public_ip"), &cdktf.TerraformOutputConfig{
-		Value: instance.PublicIp(),
+	aws.NewAwsProvider(stack, js("aws_sso"), &aws.AwsProviderConfig{
+		Region: js(region),
 	})
 
 	return stack
@@ -63,11 +51,21 @@ func main() {
 		Workspaces:   cdktf.NewNamedCloudWorkspace(js("workspaces")),
 	})
 
-	// The infra stacks under management.
-	accounts := []string{"test-1", "test-2"}
+	full_accounts := []string{"net", "log", "lib", "ops", "sec", "hub", "pub", "dev", "dmz"}
+	env_accounts := []string{"net", "lib", "hub"}
 
-	for _, acc := range accounts {
-		ws_name := fmt.Sprintf("%s-ws", acc)
+	// The infra stacks under management.
+	accounts := []string{"gyre", "curl", "coil", "helix", "spiral"}
+	regions := []string{"us-east-2", "us-west-1", "us-east-1", "us-east-2", "us-west-2"}
+	sso_regions := []string{"us-east-2", "us-west-2", "us-east-1", "us-east-2", "us-west-2"}
+	namespaces := []string{"gyre", "curl", "coil", "helix", "spiral"}
+	orgs := []string{"gyre", "curl", "coil", "helix", "spiral"}
+	prefixes := []string{"aws-", "aws-", "aws-", "aws-", "aws-"}
+	domains := []string{"defn.us", "defn.us", "defn.us", "defn.us", "defn.us"}
+	sub_accounts := [][]string{{"ops"}, env_accounts, env_accounts, full_accounts, full_accounts}
+
+	for i, acc := range accounts {
+		ws_name := acc
 
 		// Create a tfc workspace for each stack
 		workspace.NewWorkspace(workspaces, js(ws_name), &workspace.WorkspaceConfig{
@@ -77,7 +75,7 @@ func main() {
 		})
 
 		// Create the infra stack.
-		st := TheStack(app, acc)
+		st := TheStack(app, acc, regions[i], sso_regions[i], namespaces[i], orgs[i], prefixes[i], domains[i], sub_accounts[i])
 		cdktf.NewCloudBackend(st, &cdktf.CloudBackendProps{
 			Hostname:     js("app.terraform.io"),
 			Organization: js(tfc_org),
