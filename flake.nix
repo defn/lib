6d54ee7ef0df941b548efb5da2eaf086b38dc3a0
amanibhavam <iam@defn.sh>
2022-12-28 16:39:18 -0800

    update defn/lib

--- flake.nix
+++ flake.nix
@@ -3,56 +3,53 @@
     dev.url = github:defn/pkg/dev-0.0.14?dir=dev;
   };
 
-  outputs = inputs:
-    let
-      src = ./.;
-    in
-    inputs.dev.main {
-      inherit src;
-      inherit inputs;
+  outputs = inputs: inputs.dev.main rec {
+    inherit inputs;
 
-      config = rec {
-        slug = "lib";
-        version = builtins.readFile ./VERSION;
-      };
+    src = builtins.path { path = ./.; name = config.slug; };
 
-      handler = { pkgs, wrap, system, builders }:
-        let
-          src = ./.;
-          pwd = src;
-          version = builtins.readFile ./VERSION;
-          apps = [ "hello" "bye" "api" "infra" ];
-
-          goEnv = pkgs.mkGoEnv {
-            inherit pwd;
-          };
-
-          go = pkgs.lib.genAttrs apps
-            (name: pkgs.buildGoApplication {
-              inherit pwd;
-              inherit src;
-              inherit version;
-              pname = name;
-              subPackages = [ "cmd/${name}" ];
-            });
-        in
-        rec {
-          defaultPackage = wrap.nullBuilder {
-            propagatedBuildInputs = [
-              builders.yaegi
-              goEnv
-            ];
-          };
-
-          packages = pkgs.lib.genAttrs apps
-            (name: wrap.bashBuilder {
-              inherit src;
-
-              installPhase = ''
-                mkdir -p $out/bin
-                cp ${go.${name}}/bin/${name} $out/bin/lib
-              '';
-            });
-        };
+    config = rec {
+      slug = builtins.readFile ./SLUG;
+      version = builtins.readFile ./VERSION;
     };
+
+    handler = { pkgs, wrap, system, builders }:
+      let
+        inherit src;
+        pwd = src;
+        version = builtins.readFile ./VERSION;
+        apps = [ "hello" "bye" "api" "infra" ];
+
+        goEnv = pkgs.mkGoEnv {
+          inherit pwd;
+        };
+
+        go = pkgs.lib.genAttrs apps
+          (name: pkgs.buildGoApplication {
+            inherit pwd;
+            inherit src;
+            inherit version;
+            pname = name;
+            subPackages = [ "cmd/${name}" ];
+          });
+      in
+      rec {
+        defaultPackage = wrap.nullBuilder {
+          propagatedBuildInputs = [
+            builders.yaegi
+            goEnv
+          ];
+        };
+
+        packages = pkgs.lib.genAttrs apps
+          (name: wrap.bashBuilder {
+            inherit src;
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp ${go.${name}}/bin/${name} $out/bin/lib
+            '';
+          });
+      };
+  };
 }
