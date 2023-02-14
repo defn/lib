67388ed00a8718a3274c321524ebd33ab65ee8bb
amanibhavam <iam@defn.sh>
2023-02-14 03:08:29 +0000

    working

--- infra/defn/flake.nix
+++ infra/defn/flake.nix
@@ -1,8 +1,7 @@
 {
   inputs = {
     dev.url = github:defn/pkg/dev-0.0.23?dir=dev;
-    defn-lib.url = github:defn/lib/0.0.15;
-    terraform.url = github:defn/pkg/terraform-1.3.4?dir=terraform;
+    lib.url = github:defn/lib/0.0.17;
   };
 
   outputs = inputs: inputs.dev.main rec {
@@ -11,17 +10,7 @@
     src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
 
     handler = { pkgs, wrap, system, builders, commands, config }: rec {
-      defaultPackage = wrap.bashBuilder {
-        buildInputs = with pkgs; [ nodejs-18_x ] ++ wrap.flakeInputs;
-
-        inherit src;
-
-        installPhase = ''
-          mkdir -p $out
-          ${inputs.defn-lib.packages.${system}.infra}/bin/infra
-          cp -a cdktf.out/. $out/.
-        '';
-      };
+      defaultPackage = inputs.lib.cdktf { inherit src; inherit wrap; };
     };
   };
 }
