987912139c706381dd289a1fd51b7858b51a2e78
amanibhavam <iam@defn.sh>
2023-02-14 21:55:13 +0000

    sync infra and lib

--- infra/defn/flake.nix
+++ infra/defn/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.lib.url = github:defn/lib/0.0.34;
-  inputs.infra.url = github:defn/lib/infra-0.0.2?dir=cmd/infra;
+  inputs.infra.url = github:defn/lib/infra-0.0.3?dir=cmd/infra;
   outputs = inputs: inputs.lib.cdktfMain rec {
     src = ./.;
     infra = inputs.infra;
