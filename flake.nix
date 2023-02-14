da318d49447a367161e859c2a624be2a0b41ccf4
amanibhavam <iam@defn.sh>
2023-02-14 03:03:36 +0000

    provide cdktf wrapper

--- flake.nix
+++ flake.nix
@@ -1,67 +1,84 @@
 {
   inputs = {
     dev.url = github:defn/pkg/dev-0.0.23?dir=dev;
+    terraform.url = github:defn/pkg/terraform-1.3.4?dir=terraform;
   };
 
-  outputs = inputs: inputs.dev.main rec {
-    inherit inputs;
+  outputs = inputs:
+    let
+      cdktf = { src, wrap }: wrap.bashBuilder {
+        buildInputs = wrap.flakeInputs;
 
-    src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+        inherit src;
 
-    config = rec {
-      apps = [ "hello" "bye" "api" "infra" ];
-    };
-
-    handler = { pkgs, wrap, system, builders, commands, config }:
-      let
-        goEnv = pkgs.mkGoEnv {
-          pwd = src;
-        };
-
-        goCmd = pkgs.lib.genAttrs config.apps
-          (name: pkgs.buildGoApplication {
-            inherit src;
-            pwd = src;
-            version = config.version;
-            pname = name;
-            subPackages = [ "cmd/${name}" ];
-          });
-
-        deploy = {
-          deploy = wrap.bashBuilder {
-            inherit src;
-
-            installPhase = ''
-              mkdir -p $out/bin
-              cp nix-entrypoint $out/bin/
-            '';
-          };
-        };
-      in
-      rec {
-        defaultPackage = wrap.nullBuilder {
-          propagatedBuildInputs = [
-            goEnv
-            pkgs.gomod2nix
-            deploy.deploy
-          ];
-        };
-
-        packages = deploy // pkgs.lib.genAttrs config.apps
-          (name: wrap.bashBuilder {
-            inherit src;
-
-            installPhase = ''
-              mkdir -p $out/bin
-              cp ${goCmd.${name}}/bin/${name} $out/bin/${name}
-            '';
-          });
-
-        apps = pkgs.lib.genAttrs config.apps
-          (name: {
-            type = "app";
-            program = "${packages.${name}}/bin/${name}";
-          });
+        installPhase = ''
+          mkdir -p $out
+          infra
+          cp -a cdktf.out/. $out/.
+        '';
       };
-  };
+    in
+    inputs.dev.main rec {
+      inherit inputs;
+
+      src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
+
+      config = rec {
+        apps = [ "hello" "bye" "api" "infra" ];
+      };
+
+      handler = { pkgs, wrap, system, builders, commands, config }:
+        let
+          goEnv = pkgs.mkGoEnv {
+            pwd = src;
+          };
+
+          goCmd = pkgs.lib.genAttrs config.apps
+            (name: pkgs.buildGoApplication {
+              inherit src;
+              pwd = src;
+              version = config.version;
+              pname = name;
+              subPackages = [ "cmd/${name}" ];
+            });
+
+          deploy = {
+            deploy = wrap.bashBuilder {
+              inherit src;
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp nix-entrypoint $out/bin/
+              '';
+            };
+          };
+        in
+        rec {
+          defaultPackage = wrap.nullBuilder {
+            propagatedBuildInputs = with pkgs; wrap.flakeInputs ++ [
+              goEnv
+              deploy.deploy
+              gomod2nix
+              nodejs-18_x
+              packages.infra
+            ];
+          };
+
+          packages = deploy // pkgs.lib.genAttrs config.apps
+            (name: wrap.bashBuilder {
+              inherit src;
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp ${goCmd.${name}}/bin/${name} $out/bin/${name}
+              '';
+            });
+
+          apps = pkgs.lib.genAttrs config.apps
+            (name: {
+              type = "app";
+              program = "${packages.${name}}/bin/${name}";
+            });
+        };
+    };
 }
