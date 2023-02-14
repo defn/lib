80386b7c1cfd8713835d89c389cbe50713204308
amanibhavam <iam@defn.sh>
2023-02-14 21:54:03 +0000

    using cdktfMain and infra flake

--- infra/defn/flake.nix
+++ infra/defn/flake.nix
@@ -1,5 +1,8 @@
 {
-  inputs.lib.url = github:defn/lib/0.0.31;
+  inputs.lib.url = github:defn/lib/0.0.34;
   inputs.infra.url = github:defn/lib/infra-0.0.2?dir=cmd/infra;
-  outputs = inputs: inputs.lib.cdktfMain rec { src = ./.; };
+  outputs = inputs: inputs.lib.cdktfMain rec {
+    src = ./.;
+    infra = inputs.infra;
+  };
 }
