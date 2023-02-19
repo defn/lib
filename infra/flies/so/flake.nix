f1e0658821b603f0e6b6e501e6227d21d8d4cf93
amanibhavam <iam@defn.sh>
2023-02-19 08:26:43 +0000

    move flies here

---
+++ infra/flies/so/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.pkg.url = github:defn/pkg/0.0.166;
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.bashBuilder {
+      inherit src;
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp nix-* $out/bin/
+      '';
+
+      propagatedBuildInputs = [
+        ctx.pkgs.bashInteractive
+      ];
+    };
+  };
+}
