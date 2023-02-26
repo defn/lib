8e2c98cfa9c7bfd6687508a377f2669c1c2ac21e
amanibhavam <iam@defn.sh>
2023-02-26 14:03:12 -0800

    upgrade terraform, go, aws provider

--- infra/defn/flake.nix
+++ infra/defn/flake.nix
@@ -1,8 +1,9 @@
 {
-  inputs.lib.url = github:defn/lib/0.0.45;
-  inputs.infra.url = github:defn/lib/infra-0.0.7?dir=cmd/infra;
+  inputs.lib.url = github:defn/lib/0.0.47;
+  inputs.infra.url = github:defn/lib/infra-0.0.10?dir=cmd/infra;
   outputs = inputs: inputs.lib.cdktfMain rec {
     src = ./.;
     infra = inputs.infra;
+    infra_cli = "infra";
   };
 }
