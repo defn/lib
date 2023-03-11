bbe92dac0ba45098aef56a5a094cd3173a0096f7
amanibhavam <iam@defn.sh>
2023-03-11 18:57:06 +0000

    upgrade to latest lib

--- cmd/tired-proxy/flake.nix
+++ cmd/tired-proxy/flake.nix
@@ -1,8 +1,10 @@
 {
-  inputs.lib.url = github:defn/lib/0.0.50;
+  inputs.lib.url = github:defn/lib/0.0.52;
   outputs = inputs: inputs.lib.goMain rec {
     src = ./.;
 
-    extend = { };
+    extend = ctx: {
+      propagatedBuildInputs = [ ctx.pkgs.irssi ];
+    };
   };
 }
