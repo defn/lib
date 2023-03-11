6942092c26ab53306533a2d30b09eea8ceebf3c5
amanibhavam <iam@defn.sh>
2023-03-11 19:46:49 +0000

    latest

--- cmd/moria/flake.nix
+++ cmd/moria/flake.nix
@@ -1,10 +1,6 @@
 {
-  inputs.lib.url = github:defn/lib/0.0.56;
+  inputs.lib.url = github:defn/lib/0.0.57;
   outputs = inputs: inputs.lib.goMain rec {
     src = ./.;
-
-    extendShell = ctx: {
-      propagatedBuildInputs = [ ctx.pkgs.irssi ];
-    };
   };
 }
