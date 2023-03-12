08df1c4445db98a2b1dcd67cd8932894d1ecaa11
amanibhavam <iam@defn.sh>
2023-03-12 21:38:17 +0000

    convert to cobra

--- cmd/meh/flake.nix
+++ cmd/meh/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.lib.url = github:defn/lib/0.0.57;
+  inputs.lib.url = github:defn/lib/0.0.63;
   outputs = inputs: inputs.lib.goMain rec {
     src = ./.;
   };
--- cmd/meh/meh.go
+++ cmd/meh/meh.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/gofiber/fiber/v2"
-)
-
-type User struct {
-	Name string `json:"name"`
-}
-
-type QueryResult struct {
-	Results []User `json:"results"`
-}
+import "github.com/defn/lib/cmd/meh/cmd"
 
 func main() {
-	app := fiber.New()
-
-	results := QueryResult{
-		[]User{{"defn"}, {"Tolan"}, {"lamda"}, {"Hana"}},
-	}
-
-	app.Get("/meh", func(c *fiber.Ctx) error {
-		b, err := json.Marshal(results)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-
-		return c.SendString(string(b))
-	})
-
-	app.Listen(":3000")
+	cmd.Execute()
 }
