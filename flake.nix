54a13677be487104012c4b627afd83be2a80bcd3
amanibhavam <iam@defn.sh>
2023-03-13 03:02:22 +0000

    add operatorsdk

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.166;
     terraform.url = github:defn/pkg/terraform-1.4.0-0?dir=terraform;
-    godev.url = github:defn/pkg/godev-0.0.5?dir=godev;
+    godev.url = github:defn/pkg/godev-0.0.6?dir=godev;
     nodedev.url = github:defn/pkg/nodedev-0.0.1?dir=nodedev;
   };
 
